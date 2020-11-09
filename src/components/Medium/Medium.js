8754caef162a3dc1befaaf40bb770de17b6bb6ad
Steven Larsen <slarsen0823@gmail.com>
2020-11-09 12:34:50 -0700

    commit

--- src/components/Medium/Medium.js
+++ src/components/Medium/Medium.js
@@ -1,25 +1,35 @@
-import React, { Component } from 'react';
-import Card from './../shared/Card/Card';
-import Loading from './../shared/Loading/Loading';
+import React, { Component } from "react";
+import Card from "./../shared/Card/Card";
+import Loading from "./../shared/Loading/Loading";
+import { requestArticles } from "../../redux/mediumReducer";
+import { connect } from "react-redux";
 
-class Medium extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { articles: [], loading: true }
+class MediumNews extends Component {
+  componentDidMount() {
+    this.props.requestArticles();
   }
+
   render() {
-    const articles = this.state.articles.map((article => <Card key={article.id} article={article} />))
+    const articles = this.props.articles.map((article) => (
+      <Card key={article.id} article={article} />
+    ));
     return (
-      <div className='news-container'>
+      <div className="news-container">
         <img src="./mediumLogo.png" style={styles.logo} alt="" />
-        {this.state.loading ? <Loading /> : <div>{articles}</div>}
+        {this.props.loading ? <Loading /> : <div>{articles}</div>}
       </div>
-    )
+    );
   }
 }
 
-export default Medium;
+const mapStateToProps = (state) => state.medium;
+const mapDispatchToProps = {requestArticles}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MediumNews);
 
 const styles = {
-  logo: { width: '250px' }
-}
+  logo: { width: "250px" },
+};
---
+++ src/redux/hackerNewsReducer.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const initialState = {
+  articles: [],
+  loading: false,
+};
+
+const REQUEST_ARTICLES = "REQUEST_ARTICLES";
+
+export function requestArticles(){
+    let articles = axios.get('/api/hacker-news').then(res => res.data)
+    return {
+        type: REQUEST_ARTICLES,
+        payload: articles
+    }
+}
+
+export default function(state = initialState, action){
+    switch (action.type){
+        case REQUEST_ARTICLES + "_PENDING":
+            return {...state, loading: true};
+        case REQUEST_ARTICLES + "_REJECTED":
+            return {...state, loading: false};
+        case REQUEST_ARTICLES + "_FULFILLED": 
+            return {...state, loading: false, articles: action.payload};
+        default:
+            return state;
+    }
+}
